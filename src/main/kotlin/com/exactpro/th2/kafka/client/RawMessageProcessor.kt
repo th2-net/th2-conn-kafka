1b179785ce0c7ebe3e4ffa41212873637d037ef5
lumber1000 <45400511+lumber1000@users.noreply.github.com>
2024-08-07 22:31:43 +0400

    th2-gradle-plugin gradle update dependencies update workflows update

--- src/main/kotlin/com/exactpro/th2/kafka/client/RawMessageProcessor.kt
+++ src/main/kotlin/com/exactpro/th2/kafka/client/RawMessageProcessor.kt
@@ -27,7 +27,7 @@ import com.exactpro.th2.common.utils.message.id
 import com.exactpro.th2.common.utils.message.logId
 import com.exactpro.th2.common.utils.message.toTimestamp
 import com.exactpro.th2.common.utils.message.transport.logId
-import mu.KotlinLogging
+import io.github.oshai.kotlinlogging.KotlinLogging
 import java.time.Instant
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
